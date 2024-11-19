85aa53c1f0d30c10785bca9d7f64af89f788760f
Hadiku94 <jeromepeay@icloud.com>
2024-11-19 21:39:33 +0000

    Test passed again.

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
-// YOUR NAME: 
-// COLLABORATORS: 
-// DATE: 
+// YOUR NAME: Chris Peay
+// COLLABORATORS: N/A
+// DATE: 18 Nov 2024
 
 import org.apache.commons.lang3.Validate;
 
 public class Main
+
 {
 /*Made a class level variable so that all methods can call these constant variables */
          //ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
@@ -43,16 +44,26 @@ public class Main
         //extra vars to help with formatting of Strings (not raw data like above vars)
         String uscisNum, dateOfBirth, validDate, expireDate;
 
-        //INITIALIZATION SECTION
-        surname = "CHAPETON-LAMAS";
-        givenName = "NERY";
+        //INITIALIZATION SECTION NOW N/A
+
+
+
+        //INPUT SECTION
+        surname = UtilityBelt.readString("Enter surname: ", 0, 50);// "CHAPETON-LAMAS"; // flexible for long names
+        givenName = UtilityBelt.readString("Enter givin name: ", 0, 50);//"NERY";
+        category = UtilityBelt.readString("Enter category: ", 0, 3);//"C09";
+        cardNum = UtilityBelt.readString("Enter card number: ", 0, 13);//"SRC9876543210";
+        birthCountry = UtilityBelt.readString("Enter birth country: ", 0, 50);//"Guatemala";
+        termsAndConditions = UtilityBelt.readString("Enter terms and conditions: ", 0, 50);//"None";
+        sex = UtilityBelt.readChar("Enter Sex: ", "FfMmNnIi");//'M'; //note single quotes
+
 
         uscisNum1 = UtilityBelt.readInt( "Enter the USCIS # PART 1: " , 0,  999);//12; value to remember
         uscisNum2 = UtilityBelt.readInt( "Enter the USCIS # PART 2: " , 0,  999);//4;
-        uscisNum3 = UtilityBelt.readInt( "Enter the USCIS # PART 3: " , 0,  999);//789;
+        uscisNum3 = UtilityBelt.readInt( "Enter the USCIS # PART 3: " , 0,  999);//789; 
         
 
-        birthMonth = "JAN";
+        birthMonth = UtilityBelt.readString("Enter birth month(as 3 letter acronym in ALL CAPS): ", 3, 3);//"JAN";// 3 
         birthDay = UtilityBelt.readInt( "Enter birth day: " , 1,  30);//1;
         birthYear = UtilityBelt.readInt( "Enter birth year: " , 1700,  2024);//1970;//more specific for years
         
@@ -63,18 +74,6 @@ public class Main
         expireMonth = UtilityBelt.readInt( "Enter expiration month: " , 1, 12);//2;//2;
         expireDay = UtilityBelt.readInt( "Enter expirarion day: " , 1,  30);//2;
         expireYear = UtilityBelt.readInt( "Enter expiration year: " , 1000,  9999);// 2022;
-       
-        category = "C09";
-        cardNum = "SRC9876543210";
-        birthCountry = "Guatemala";
-        termsAndConditions = "None";
-        sex = 'M'; //note single quotes
-
-        
-        
-
-       
-
 
 
 
@@ -89,35 +88,39 @@ public class Main
         //N/A
 
         //OUTPUT SECTION
-        Main.formatCard(surname, givenName, category, cardNum, birthCountry, termsAndConditions, sex, uscisNum, dateOfBirth, validDate, expireDate);
-       
+        String card = Main.formatCard(surname, givenName, category, cardNum, birthCountry, termsAndConditions, sex, uscisNum, dateOfBirth, validDate, expireDate);
+        System.out.println(card);
     }
 
-    public static void formatCard(String surname, String givenName, //header
+    public static String formatCard(String surname, String givenName, //header
     String category, String cardNum, String birthCountry, 
     String termsAndConditions, char sex, String uscisNum, 
     String dateOfBirth, String validDate, String expireDate)
-    {
+    { 
         System.out.println("TEST: formatCard method called");
-        System.out.printf("╔══════════════════════════════════════════════════════════════════════╗%n");
-        System.out.printf("║%35s%35s║%n", TITLE_USA, "");
-        System.out.printf("║%60s%10s║%n", TITLE_EAC, "");
-        System.out.printf("║%-25s%-45s║%n", "", LABEL_SURNAME);
-        System.out.printf("║%-25s%-45s║%n", "", surname);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_0, LABEL_GIVEN_NAME);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_1, givenName);
-        System.out.printf("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_2, LABEL_USCIS_NUM, LABEL_CATEGORY, LABEL_CARD_NUM);
-        System.out.printf("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_3, uscisNum, category, cardNum);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_4, LABEL_BIRTH_COUNTRY);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_5, birthCountry);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_6, LABEL_TERMS_CONDITIONS);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_7, termsAndConditions);
-        System.out.printf("║%-25s%-15s%-30s║%n", ASCII_ART_8, LABEL_BIRTH_DATE, LABEL_SEX);
-        System.out.printf("║%-25s%-15s%-30s║%n", ASCII_ART_9, dateOfBirth, sex);
-        System.out.printf("║%-25s%-15s%-30s║%n", "", LABEL_VALID_DATE, validDate);
-        System.out.printf("║%-25s%-15s%-30s║%n", "", LABEL_EXPIRE_DATE, expireDate);
-        System.out.printf("║%-25s%-45s║%n", ASCII_CREDIT, LABEL_REENTRY_DISCLAIMER);
-        System.out.printf("╚══════════════════════════════════════════════════════════════════════╝%n");
+        String card = "╔══════════════════════════════════════════════════════════════════════╗\n";
+        card += String.format("║%35s%35s║%n", TITLE_USA, "");
+
+        //System.out.printf("╔══════════════════════════════════════════════════════════════════════╗%n");
+        //System.out.printf("║%35s%35s║%n", TITLE_USA, "");
+        card += String.format("║%60s%10s║%n", TITLE_EAC, "");
+        card += String.format("║%-25s%-45s║%n", "", LABEL_SURNAME);
+        card += String.format("║%-25s%-45s║%n", "", surname);
+        card += String.format("║%-25s%-45s║%n", ASCII_ART_0, LABEL_GIVEN_NAME);
+        card += String.format("║%-25s%-45s║%n", ASCII_ART_1, givenName);
+        card += String.format("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_2, LABEL_USCIS_NUM, LABEL_CATEGORY, LABEL_CARD_NUM);
+        card += String.format("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_3, uscisNum, category, cardNum);
+        card += String.format("║%-25s%-45s║%n", ASCII_ART_4, LABEL_BIRTH_COUNTRY);
+        card += String.format("║%-25s%-45s║%n", ASCII_ART_5, birthCountry);
+        card += String.format("║%-25s%-45s║%n", ASCII_ART_6, LABEL_TERMS_CONDITIONS);
+        card += String.format("║%-25s%-45s║%n", ASCII_ART_7, termsAndConditions);
+        card += String.format("║%-25s%-15s%-30s║%n", ASCII_ART_8, LABEL_BIRTH_DATE, LABEL_SEX);
+        card += String.format("║%-25s%-15s%-30s║%n", ASCII_ART_9, dateOfBirth, sex);
+        card += String.format("║%-25s%-15s%-30s║%n", "", LABEL_VALID_DATE, validDate);
+        card += String.format("║%-25s%-15s%-30s║%n", "", LABEL_EXPIRE_DATE, expireDate);
+        card += String.format("║%-25s%-45s║%n", ASCII_CREDIT, LABEL_REENTRY_DISCLAIMER);
+        card += String.format("╚══════════════════════════════════════════════════════════════════════╝");
+        return card;
     }
     
 
